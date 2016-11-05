bc4d28c3969929544d523bde04ddf3226d05fde8
Raymel Francisco <franciscoraymel@gmail.com>
2016-11-05 20:10:45 +0800

    Makes installation location dynamic for context menu link

--- src/add-to-context-menu.bat
+++ src/add-to-context-menu.bat
@@ -4,10 +4,13 @@
 ::
 :: (c) April 2015 Raymel Francisco
 ::
+:: Modified: Nov 2016
+@echo off
 
-set location=%1
+set location=%~dp0open-with-cmd.exe
 
 reg add "HKEY_CLASSES_ROOT\*\shell\Open with command prompt" /t REG_SZ /v "" /d "Open with command prompt" /f
-reg add "HKEY_CLASSES_ROOT\*\shell\Open with command prompt" /t REG_EXPAND_SZ /v "Icon" /d "%location%,0" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Open with command prompt" /t REG_EXPAND_SZ /v "Icon" /d "%~dp0terminal.ico,0" /f
 reg add "HKEY_CLASSES_ROOT\*\shell\Open with command prompt\command" /t REG_SZ /v "" /d "%location% \"%%1\"" /f
 
+pause
