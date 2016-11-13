2f54a83f26086c4bfb3ac5e40ad5943ef3d58089
Raymel Francisco <franciscoraymel@gmail.com>
2016-11-13 17:54:03 +0800

    Delete add-cleaner-to-startup.bat

--- src/add-cleaner-to-startup.bat
+++
@@ -1,7 +0,0 @@
-::
-:: add-cleaner-to-startup.bat
-:: Bypasses default powershell scripts execution policy, then runs the powershell script
-::
-:: (c) May 2015 Raymel Francisco
-::
-powershell.exe -noprofile -executionpolicy bypass -file add-cleaner-to-startup.ps1
