b4136550fda0e2b55d06046119a11e20079f5930
Raymel Francisco <franciscoraymel@gmail.com>
2016-11-13 17:51:47 +0800

    Delete compile-uninstall.bat

--- compile-uninstall.bat
+++
@@ -1,9 +0,0 @@
-::
-:: compile-uninstall.bat
-:: Compiles uninstall.c to exe file using gcc 4.8.1
-::
-:: (c) May 2015 Raymel Francisco
-::
-@windres src/uninstall.rc -O coff -o res/uninstall.res
-@gcc -m32 -o bin/uninstall.exe src/uninstall.c res/uninstall.res
-@pause
